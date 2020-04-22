4270d0ec9a0d35cef16b0d7246040a9265790408
ellie-yun <63767717+ellie-yun@users.noreply.github.com>
2020-04-21 22:57:17 -0700

    Initial Commit
    Create a tab-delimited text file by reading data from mysql database

---
+++ Textfile/data_to_file.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    user='eugenet', password='password',
+    host='ix-dev.cs.uoregon.edu', database='my_db',
+    port = "3226"
+)
+
+my_database = connection.cursor()
+sql_statement = "SELECT * FROM locationdata2"
+    
+my_database.execute(sql_statement)
+output = my_database.fetchall()
+
+with open("data_output.txt", 'w') as df:
+    for line in output:
+        userid = line[1]
+        date = line[5].replace("-","/")[0:10]
+        time = line[5][11:]
+        latitude = line[3]
+        longitude = line[2]
+
+        df.write("%s\t%s\t%s\t%s\t%s\n"
+                 % (userid, date, time, latitude, longitude))
+
+connection.close()
