3f932e933e5d7aa7aa0102a4b0d1257b86f0b178
emstrubl <63114742+emstrubl@users.noreply.github.com>
2020-04-23 22:34:06 -0700

    Updated Textfile - added conversion of data to csv file

--- Textfile/data_to_file.py
+++ Textfile/data_to_file.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import csv
 
 connection = mysql.connector.connect(
     user='eugenet', password='password',
@@ -12,7 +13,8 @@ sql_statement = "SELECT * FROM locationdata2"
 my_database.execute(sql_statement)
 output = my_database.fetchall()
 
-with open("data_output.txt", 'w') as df:
+with open("data_output.txt", 'w') as tabdfile, open("data_output.csv","w") as csvfile:
+    csvwriter = csv.writer(csvfile)
     for line in output:
         userid = line[1]
         date = line[5].replace("-","/")[0:10]
@@ -21,7 +23,11 @@ with open("data_output.txt", 'w') as df:
         longitude = line[2]
         time_at_location = line[6]
 
-        df.write("%s\t%s\t%s\t%s\t%s\t%s\n"
+
+        tabdfile.write("%s\t%s\t%s\t%s\t%s\t%s\n"
                  % (userid, date, time, latitude, longitude, time_at_location))
-    
+        csvwriter.writerow([str(userid), str(date), str(time), str(latitude), str(longitude),
+                            str(time_at_location)])
+        
+
 connection.close()
