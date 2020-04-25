009e8efbc0603eb31831a7992199238910beeab3
Eugene Tan <44623905+eugenetan3@users.noreply.github.com>
2020-04-25 00:44:39 -0700

    Commented relevant web implementation

--- flask_app.py
+++ flask_app.py
@@ -2,11 +2,11 @@ import flask
 import time
 from flask import Flask, request, jsonify, render_template, Response, redirect, url_for
 import json
-##
 import mysql.connector
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__) #Alias Flask App
 
+#Default webpage, is just a blank render template
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -15,24 +15,28 @@ def index():
 
 @app.route("/post-requests", methods=['GET', 'POST'])
 def post_request(comments=[]):
-##
 
+    #Form MYSQL database connection to database hosted on ix
     mydb = mysql.connector.connect(host="ix-dev.cs.uoregon.edu", user="eugenet", passwd="password", database="my_db", port="3226")
     mycursor = mydb.cursor()
 
+    #SQL command format to insert data 
     #add_data = ("INSERT INTO locationdata "
     #           "(id, data) "
     #           "VALUES (%s, %s)")
 
+    #SQL command to insert data taken from app
     add_data = ("INSERT INTO locationdata2 "
                "(id, user_id, longitude, latitude, speed, timestamp, time_spent) "
                "VALUES (%s, %s, %s, %s, %s, %s, %s)")
 
-
-##
+    #POST request to get JSON data that is posted to this url
     data = request.get_json()
-    comments.append(data)
-    #comments.append(mydb.is_connected())
+    comments.append(data) #Add JSON data to local python list comments
+    
+    #debugging statement: comments.append(mydb.is_connected())
+    
+    #if data collected is not None (when page is refreshed it performs a GET), then assign JSON dictionary key values to variables
     if (data != None):
         userid = data['User ID']
         longitude = data['Longitude']
@@ -42,14 +46,14 @@ def post_request(comments=[]):
         time_spent = data['Time Spent']
 
         #mysqldata = ('0', str(data))
-        mysqldata = ('0', userid, longitude, latitude, speed, date, time_spent)
+        mysqldata = ('0', userid, longitude, latitude, speed, date, time_spent) #Form a tuple with JSON data
 
 
-        mycursor.execute(add_data, mysqldata)
-        mydb.commit()
+        mycursor.execute(add_data, mysqldata) #Execute the SQL command add_data with the tuple mysqldata
+        mydb.commit() #Commit the data so it is saved
 
     if request.method == "GET":
-        return render_template("index.html", comments=comments)
+        return render_template("index.html", comments=comments) #Render comments or valid entries
     ##
 
     ##
