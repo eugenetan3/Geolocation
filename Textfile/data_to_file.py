cffbcf5441f4ed298ceedb028676cef878b5db2e
emstrubl <63114742+emstrubl@users.noreply.github.com>
2020-04-26 13:10:50 -0700

    Added comments to Textfile

--- Textfile/data_to_file.py
+++ Textfile/data_to_file.py
@@ -17,41 +17,52 @@ Course: CIS 422 - Software Methodology Project 2 under Professor Anthony Hornof
 import mysql.connector
 import csv
 
+#establish a connection to the ix-dev MySQL database
 connection = mysql.connector.connect(
     user='eugenet', password='password',
     host='ix-dev.cs.uoregon.edu', database='my_db',
     port = "3226"
 )
 
+
 my_database = connection.cursor()
+#statement to receive all data in 
 sql_statement = "SELECT * FROM locationdata2"
-    
+
+#execute the sql statement and put it into a list of strings 
 my_database.execute(sql_statement)
 output = my_database.fetchall()
 
+print(type(output))
 #data from output fixed into desired format and order
 data_array = [None] * len(output)
 
+#write the tab delimited text file
 with open("output.txt", 'w') as tabdfile:
+    #iterate over the output data
     i = 0
     for line in output:
+        #format the strings
         userid = line[1]
         date = line[5].replace("-","/")[0:10]
         time = line[5][11:]
         latitude = line[3]
         longitude = line[2]
         time_at_location = line[6]
-            
+
+        #insert the data into array for sorting
         data_array[i] = [userid, date, time, latitude, longitude, time_at_location]
         i += 1
-        
+
+        #write to text file
         tabdfile.write("%s\t%s\t%s\t%s\t%s\t%s\n"
          % (userid, date, time, latitude, longitude, time_at_location))
 
-    
+
+#sort the data array
 data_array = sorted(data_array, key = lambda x: x[0])
 
-
+#write sorted data to csv file
 with open("output.csv","w") as csvfile:
     csvwriter = csv.writer(csvfile)
     for row in data_array:
